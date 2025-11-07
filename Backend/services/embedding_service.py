30932d049a398f29c3f7ed72aad1392e82013cd0
Amrinder Singh <amrindersingh01084@gmail.com>
2025-11-07 13:47:40 -0500

    Add llm embeddint and req file

---
+++ Backend/services/embedding_service.py
@@ -0,0 +1,13 @@
+import json
+from Backend.config.bedrock_config import bedrock
+
+def get_embedding(text: str):
+    body = json.dumps({"inputText": text})
+    response = bedrock.invoke_model(
+        modelId="amazon.titan-embed-text-v2:0",
+        body=body,
+        contentType="application/json",
+        accept="application/json"
+    )
+    result = json.loads(response["body"].read())
+    return result["embedding"]
